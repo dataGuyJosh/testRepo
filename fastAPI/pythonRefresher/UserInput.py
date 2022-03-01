9891960ff056e4524d8ad130b35f93d1b4026d5b
Joshua Garvey <joshua.garvey@pointduty.com.au>
2022-03-01 11:53:10 +1000

    moving FastAPI work to separate repo

--- fastAPI/pythonRefresher/UserInput.py
+++
@@ -1,4 +0,0 @@
-"""User Input"""
-
-weeks = round(int(input("How many days until your birthday: "))/7, 2)
-print(f"Only {weeks} weeks until your birthday!")
--- fastAPI/pythonRefresher/Variables.py
+++
@@ -1,8 +0,0 @@
-"""
-Variables
-"""
-first_name="Eric"
-print(first_name)
-
-first_name="Melissa"
-print("This is "+first_name)
